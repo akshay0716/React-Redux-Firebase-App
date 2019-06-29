d5fc4a139288ea3cbf4d959ffe64a86c517ab39f
Akshay Nagargoje <akshay.nagargoje@myrl.tech>
2019-06-29 20:03:51 +0530

    akshay changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default App
